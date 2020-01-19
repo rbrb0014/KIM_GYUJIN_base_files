8c84c09a16d8e30df029201291195ab73eddd257
김규진 <rbrb0014@gmail.com>
2020-01-19 16:05:37 +0900

    tast 20200119

--- Folder_c/code.c
+++ Folder_c/code.c
@@ -5,10 +5,14 @@ void main()
     printf("hello C-lang!\n");
     if (1)
     {
-        printf("good");
+        printf("yes!");
     }
     else
     {
         printf("bad");
     }
 }
+
+
+//ctrl+shift+B  gcc_compile     build
+//ctrl+P    task execute_c      execute
