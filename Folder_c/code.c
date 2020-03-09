0d0891781246c50b2cbb08efdc1814af4c816421
김규진 <rbrb0014@gmail.com>
2020-03-09 15:51:55 +0900

    simsim

--- Folder_c/code.c
+++ Folder_c/code.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
+#define MAX 503
 
-void main()
+int map[MAX][MAX];
+int n, m;
+
+int bfs(int s1, int s2)
 {
-    printf("hello C-lang!\n");
-    if (1)
-    {
-        printf("yes!");
-    }
-    else
-    {
-        printf("bad");
-    }
+    if (s1 == n && s2 == m)
+        return 1;
+    
+    int cnt = 0;
+    if (0 < map[s1 - 1][s2] && map[s1 - 1][s2] < map[s1][s2])
+        cnt += bfs(s1 - 1, s2);
+    if (0 < map[s1 + 1][s2] && map[s1 + 1][s2] < map[s1][s2])
+        cnt += bfs(s1 + 1, s2);
+    if (0 < map[s1][s2 - 1] && map[s1][s2 - 1] < map[s1][s2])
+        cnt += bfs(s1, s2 - 1);
+    if (0 < map[s1][s2 + 1] && map[s1][s2 + 1] < map[s1][s2])
+        cnt += bfs(s1, s2 + 1);
+    return cnt;
 }
 
+int main()
+{
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            scanf("%d", &map[i][j]);
+        }
+    }
+    printf("%d", bfs(1, 1));
+
+    return 0;
+}
 
 //ctrl+shift+B  gcc_compile     build
 //ctrl+P    task execute_c      execute
--- Foloer_python/test.py
+++ Foloer_python/test.py
@@ -1,1 +1,14 @@
-print("wow")
+import smtplib
+from email.mime.text import MIMEText
+
+smtp = smtplib.SMTP('smtp.nave.com',465)
+smtp.ehlo()
+smtp.starttls()
+smtp.login('rbrb0014','qawsedrf4827@')
+
+msg = MIMEText('write your message here')
+msg['Subject'] = '아니 이거 왜 작동 안해'
+msg['To'] = 'kim@naver.com'
+smtp.sendmail('rbrb0014@naver.com','rbrb0014@gmail.com',msg.as_string())
+
+smtp.quit()
