63c67af94dd74a1905502cc64d6abc36852979ae
김규진 <rbrb0014@gmail.com>
2020-04-27 06:25:32 +0900

    verilog hw

---
+++ Folder_verilog/num_7seg_B.v
@@ -0,0 +1,22 @@
+module num_7seg_B(out,w,x,y,z);
+/*
+    about Fb();
+    0000 1
+    0001 0
+    0010 0
+    0011 0
+    0100 1
+    0101 1
+    0110 1
+    0111 0
+    1000 1
+    1001 1
+*/
+input w,x,y,z;
+output out;
+
+wire w,x,y,z,out;
+
+assign out=(~y & ~z)|(x & ~z)|(x & ~y)|w;
+
+endmodule
