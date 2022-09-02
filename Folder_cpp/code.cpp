3424d655093bb056d463ffccdfeb91fcae40f0bf
rbrb0014 <rbrb0014@gmail.com>
2022-09-02 15:32:13 +0900

    cync1

--- Folder_cpp/code.cpp
+++ Folder_cpp/code.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 vector<vector<int>> adj;
 
-
-int main (){
-    int n, in1,in2;
+int main()
+{
+    int n, in1, in2;
     printf("how many nodes there? : ");
     scanf("%d", n);
     adj.resize(n);
 
     printf("write the edge like n m. if finished, input 0.\n");
-    for(int i=0;i<n;i++){
-        printf("%d",scanf("%d %d",in1,in2));
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", scanf("%d %d", in1, in2));
     }
-
 }
 
-//ctrl+shift+B  gcc_compile     build
-//ctrl+P    task execute_c      execute
+// ctrl+shift+B  gcc_compile     build
+// ctrl+P    task execute_c      execute
